bea77d51264c926551316766df264cb2d4bcd75b
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-08-12 06:29:02 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://eb.birn.cc",
+  base:"/astro-test",
   markdown: {
     remarkPlugins: [addLayoutFrontmatter],
   },
